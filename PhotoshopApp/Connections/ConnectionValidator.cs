36aba6f831771a52f8083d247b887a7ed11f1132
Alex Terehov <1920639+terales@users.noreply.github.com>
2025-02-22 08:44:32 -0330

    prepare connection for local processing

--- PhotoshopApp/Connections/ConnectionValidator.cs
+++ PhotoshopApp/Connections/ConnectionValidator.cs
@@ -1,45 +1,16 @@
 using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Connections;
-using RestSharp;
-using PhotoshopApp.Constants;
-using PhotoshopApp.Models.Dto;
-using PhotoshopApp.Models.Response;
-using PhotoshopApp.RestSharp;
 
 namespace PhotoshopApp.Connections;
 
-/// <summary>
-/// Validates app credentials that were provided by the user
-/// </summary>
 public class ConnectionValidator : IConnectionValidator
 {
-    private static readonly AppRestClient Client = new();
-    
-    public async ValueTask<ConnectionValidationResponse> ValidateConnection(
+    public ValueTask<ConnectionValidationResponse> ValidateConnection(
         IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
     {
-        var request = new AppRestRequest(ApiEndpoints.Berry, Method.Get, authProviders);
-
-        try
+        return new(Task.FromResult(new ConnectionValidationResponse()
         {
-            await Client.ExecuteWithHandling<ListResponse<Berry>>(request);
-
-            // If ExecuteWithHandling did not throw an exception
-            // then request was successful and credentials are valid
-            return new()
-            {
-                IsValid = true
-            };
-        }
-        catch (Exception ex)
-        {
-            // If Exception was thrown, we mark connection credentials as invalid
-            // and pass the exception message in the response
-            return new()
-            {
-                IsValid = false,
-                Message = ex.Message
-            };
-        }
+            IsValid = true
+        }));
     }
 }
